6ce7c360dc2efd6c8d191205770b59e537a25dc0
root <root@ip-172-31-20-214.us-west-2.compute.internal>
2023-11-11 18:32:05 +0000

    test21

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Sanjay" }');
+    res.send('{ "response": "Hello From Sanjay Chettri" }');
 });
 
 app.get('/will', function (req, res) {
